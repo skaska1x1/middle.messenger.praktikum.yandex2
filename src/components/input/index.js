bb9b9dcd2b0e1d34197bf33b6cd2747cd77bf438
skaska1x1 <skaska1x1@ya.ru>
2022-07-26 21:01:49 +0300

    just readme

--- src/components/input/index.js
+++
@@ -1,9 +0,0 @@
-import Handlebars from "handlebars";
-import tpl from "./tpl.hbs";
-import "./style.css";
-
-Handlebars.registerPartial('input', tpl)
-
-export default (id, value) => {
-    return Handlebars.compile(tpl)({ id, value })
-}
