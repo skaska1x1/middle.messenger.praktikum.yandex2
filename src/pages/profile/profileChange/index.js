daacbe7472600f0f91d43d617d13d0200fd2a6f6
skaska1x1 <skaska1x1@ya.ru>
2022-07-30 19:33:01 +0300

    удалил page1 page2 , удалил комментарии

--- src/pages/profile/profileChange/index.js
+++ src/pages/profile/profileChange/index.js
@@ -1,5 +1,4 @@
 import Handlebars from 'handlebars';
-//import Handlebars from 'handlebars/dist/handlebars.runtime';
 import tpl from './tpl.hbs';
 import './style.css';
 import button from '../../../components/button';
